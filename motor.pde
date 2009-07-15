693b964e5e122a3683f31e43b8b86280bf3967a2
simonz05 <simonz05@aa7e8778-619f-11de-b7a8-4be610c0df87>
2009-07-15 18:04:31 +0000

    lots of changes. Especially implementation of Kalman filter and motor controll

---
+++ motor.pde
@@ -0,0 +1,48 @@
+/*
+  To get the Newton meter of motors we take motors kg meter value multiply 9.8^2 
+ [0.002548 kg m * 9.8m/s^2 = 0.24471 N m] This is the Motors torque, its strenght.
+ [(0.002548 kg m * 2) * 9.8m/s^2 = 0.48942 N m] (for two motors).
+ Since we have wheels the we'll have to find the force at the lever arm (force at wheel)
+ Formula: Force at wheels = torque / radius. 
+ [0.24471 N m / 0.021 m = 11.6529 N]
+ [0.48942 N m / 0.021 m = 23.3057 N] (for two motors)
+ (note: Adding bigger wheels might be smart, bigger wheels will drive the robot faster, 
+ thus lowering the overal torque) 
+ To level the robot at a given angle we'll have to know the height of the robot
+ so we can find the force we need to counter it. 
+ Torque applied [Force at wheels * length * cos(degrees)] has to be bigger than
+ the torque applied by gravity to overcome it [mass * gravity * length * sin(degrees)]
+ 
+ Torque: Force * Radius
+ 
+ Example I: if the angle of the robot is 10 degrees the torque applied by gravity is
+ [0.650 kg * 9.8 m/s * 0.22 L * sin(10) = 0.243351 N m]
+ 1.17924
+ Lets now calculate the torque we need to counter this.
+ [11.6529 * 0.22 L * cos(10) = 2.52469 N]
+ [23.3057 * 0.22 L * cos(10) = 5.04936 N] (for two motors)
+ 
+ Example II: trying out some new formulas; cant make sence of earlier calc.
+ 0.650 Kg  * 9.8 m/s * 0.22 L * (radians(20 degree) * PI) = 0.569751 N m
+ 20 degree in radians is 0.3491. using the above formula gives me a proportional
+ incline based on degree of robot. 
+ */
+ 
+#include "parameters.h"
+
+void driveMotors(int pwm)
+{
+    if(pwm >= 0){
+        pwm = constrain(pwm, 0, 255);
+	pwm = map(pwm, 0, 255, 0, 127);        
+        motor.motor0Reverse(pwm);
+        motor.motor1Reverse(pwm);
+    }
+    else{
+        pwm = abs(pwm);
+        pwm = constrain(pwm, 0, 255);
+	pwm = map(pwm, 0, 255, 0, 127);
+        motor.motor0Forward(pwm);
+        motor.motor1Forward(pwm);
+    }
+}
---
+++ parameters.h
@@ -0,0 +1,118 @@
+///////////////////////////////////////////////////////////////////////
+//////////////////////                         ////////////////////////
+//////////////////////       parameters.h      ////////////////////////
+//////////////////////                         ////////////////////////
+///////////////////////////////////////////////////////////////////////
+//#include "globals.h"
+//
+//		Parameters required for operation and tuning go here
+//
+//
+//#define 	CPUCLK_PARAM   		8000000		//	The speed of the cpu currently in use
+
+///////////////////////////////////////////////////////////////////////
+//	Application Specific Parameters
+///////////////////////////////////////////////////////////////////////
+//
+//	Balancing Bot 
+//
+//	dt_PARAM is how often the Kalman state is updated with
+//	gyro rate measurements.  You will have to
+//	change this value if you update at a different rate.
+//	For example, the 50Hz is based upon the call frequency
+//	of balance() from make_it_balance().
+//	If xFrequency is changed in make_inBalance() then this
+//	should be changed and vice versa.
+//
+//	40Hz Rate for debugging because of so many rprintfs 
+//define		dt_PARAM		0.100		//	10 Hz rate
+//#define		dt_PARAM		0.01		//	100 Hz rate
+#define		dt_PARAM		0.02		//	50 Hz rate
+#define 	my_dt_PARAM 	20 		// 50 Hz rate
+
+//	R may be used for tuning Kalman
+//	R represents the measurement covariance noise.  In this case,
+//	it is a 1x1 matrix that says that we expect 0.3 rad jitter
+//	from the accelerometer.
+//
+//	Adjusting R changes the speed at which the filter converges.
+//	It is an indication of trust in the measurement
+#define		R_angle_PARAM		.2			//	n rad jitter  / .3
+//  #define R_angle 0.4 // 0.0034906555555555555556; // 0.3;
+
+//	Q may be used for tuning Kalman
+//	Q is a 2x2 matrix that represents the process covariance noise.
+//	In this case, it indicates how much we trust the acceleromter
+//	relative to the gyros.
+//
+// 	originally .001 and .003
+#define		Q_angle_PARAM		0.001
+#define		Q_gyro_PARAM		0.001
+
+
+// These are measured values.  They will change with sensor 
+// orientation, so be careful to change these when necessary.
+//
+// value at 0 degrees / sec - unimportant, as the Kalman filter corrects this
+//#define 	GYRO_OFFSET 		613		
+#define 	GYRO_OFFSET 		580
+		
+// value at 0 degrees  / 1 unit .4 degees  (move to +90 -90 / 2)
+//#define 	ACCEL_OFFSET 		650	
+//#define 	ACCEL_OFFSET 		540	
+#define 	ACCEL_OFFSET		744   // 647, 733, 744
+
+// gyro is 2 mv/(deg/sec), ADC is 2.5mv/tick: 2/2.5 ; degrees/sec = adc * .8 
+// gyro is 2 mv/(deg/sec), ADC is 3.22mv/tick: 2/3.3  ; degrees/sec = adc * .62
+// ADXRS401 gyro is 15mv/(deg/sec), ADC is 4.88mv/bit: 15/4.88  ; degrees/sec = adc * 3.07
+#define 	GYRO_SCALE 			3.07
+
+
+// ADC bits per 90 degrees
+#define 	ACCEL_SCALE 		0x0d8	
+//#define 	ACCEL_SCALE 		106.0	
+
+
+//	Neutral or Natural balance angle
+//	You might change this by .1 at a time until tuned    
+//#define	neutral_PARAM 	-7.7		// "angle of natural balance"  
+#define		neutral_PARAM 	-2			// "angle of natural balance"  
+
+//	Motor Bias - left and right
+#define		mb_PARAM 	5		//	PWM required to get the motor turning
+#define MAX_TORQUE		127
+
+// 	PID adjustable gains
+//
+//	Proportional Constant
+#define		Kp_PARAM 			200			// balance loop P gain
+
+//	Differential Constant
+//	You might change this by .1 to .5 at a time until tuned // 12.5
+//	#define		Kd_PARAM	 		22	// balance loop D gain -- good for 8.4 volt lipol
+#define		Kd_PARAM	 		12		// balance loop D gain -- good for 8.4 volt lipol
+#define RAD_TO_DEG	(180 / 3.1415926535897932384626433832795)
+
+
+//	Robot statistics. Can be used for calculating torque.
+#define rob_weight_PARAM	0.650	 // Weight of robot measured in kilogram.
+#define rob_lenght_PARAM 	0.22 	 // Height of robot measured in meter.
+#define rob_wheel_PARAM 	0.021	 // Radius of Wheels measured in meter.
+#define rob_motor_kg_PARAM	0.002548 // Torque of motors measured in kilogram meter.
+#define gravity_PARAM		9.8 	 // m/s gravity. This should be 0.098 if mydt is 10. 
+
+
+//  Digital Pins on Arduino
+#define rx_pin 		3
+#define tx_pin 		4
+#define rst_pin 	5
+#define led1_pin 	13
+#define led2_pin 	12
+
+
+//  Analog Pins on Arduino
+#define x_pin 		0
+#define y_pin 		1
+#define gyro_pin 	2 
+#define gyroref_pin 3
+#define pot_pin 	4
